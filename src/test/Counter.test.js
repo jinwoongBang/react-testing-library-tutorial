1bef8552e114f5f66ee5868a3561658a912a9025
jinwoongBang <dlopo123@gmail.com>
2020-12-15 13:35:59 +0900

    클릭 이벤트 테스트 코드 추가

--- src/test/Counter.test.js
+++ src/test/Counter.test.js
@@ -5,6 +5,7 @@ import Counter from "../components/Counter";
 describe("<Counter />", () => {
   it("matches snapshot", () => {
     const utils = render(<Counter />);
+    // 이전에 테스트 했던 것과 동일한지 테스트
     expect(utils.container).toMatchSnapshot();
   });
   it("has a number and two buttons", () => {
@@ -13,5 +14,25 @@ describe("<Counter />", () => {
     utils.getByText("0");
     utils.getByText("+1");
     utils.getByText("-1");
+  });
+  it("increases", () => {
+    const utils = render(<Counter />);
+    const number = utils.getByText("0");
+    const plusButton = utils.getByText("+1");
+    // 클릭 이벤트 두번 발생시키기
+    fireEvent.click(plusButton);
+    fireEvent.click(plusButton);
+    expect(number).toHaveTextContent("2"); // jest-dom 의 확장 matcher 사용
+    expect(number.textContent).toBe("2"); // textContent 를 직접 비교
+  });
+  it("decreases", () => {
+    const utils = render(<Counter />);
+    const number = utils.getByText("0");
+    const minusButton = utils.getByText("-1");
+    // 클릭 이벤트 두번 발생시키기
+    fireEvent.click(minusButton);
+    fireEvent.click(minusButton);
+    expect(number).toHaveTextContent("-2"); // jest-dom 의 확장 matcher 사용
+    expect(number.textContent).toBe("-2"); // textContent 를 직접 비교
   });
 });
