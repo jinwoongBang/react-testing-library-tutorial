1c01fdc511f28bace4b708607db58aeb3f51a610
jinwoongBang <dlopo123@gmail.com>
2020-12-15 11:47:09 +0900

    카운터 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Profile from "./components/Profile";
+import Counter from "./components/Counter";
 
 function App() {
-  return <Profile username="velopert" name="김민준" />;
+  // return <Profile username="velopert" name="김민준" />;
+  return <Counter />;
 }
 
 export default App;
