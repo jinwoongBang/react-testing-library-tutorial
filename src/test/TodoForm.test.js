4de116d6d85819cf1f4c8aa1405e0607c23f38f8
jinwoongBang <dlopo123@gmail.com>
2020-12-15 13:47:43 +0900

    테스트 를 통과하는 컴포넌트 작성

--- src/test/TodoForm.test.js
+++ src/test/TodoForm.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import TodoForm from "../components/TodoForm";
 
-describe(<TodoForm />, () => {
+describe("<TodoForm />", () => {
   it("has input and a button", () => {
     const { getByText, getByPlaceholderText } = render(<TodoForm />);
     getByPlaceholderText("할 일을 입력하세요"); // input 이 있는지 확인
