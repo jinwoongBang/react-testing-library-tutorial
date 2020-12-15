4ae0e3171e70a7be79e245e08d8d1aeeebf4e6ae
jinwoongBang <dlopo123@gmail.com>
2020-12-15 11:41:25 +0900

    CRA 에 테스팅 라이브러리가 포함되어있어 이전에 설치한 노드 모듈(라이브러리) 삭제

---
+++ src/Profile.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Profile from "./components/Profile";
+
+describe("<Profile />", function () {
+  it("matches snapshot", function () {
+    const utils = render(<Profile username="velopert" name="김민준" />);
+    expect(utils.container).toMatchSnapshot();
+  });
+  it("shows the props correctly", () => {
+    const utils = render(<Profile username="velopert" name="김민준" />);
+    utils.getByText("velopert"); // velopert 라는 텍스트를 가진 엘리먼트가 있는지 확인
+    utils.getByText("(김민준)"); // (김민준) 이라는 텍스트를 가진 엘리먼트가 있는지 확인
+    utils.getByText(/김/); // 정규식 /김/ 을 통과하는 엘리먼트가 있는지 확인
+  });
+});
