f11cecfd8d6fa3682700f583fde54a8cb949ab3e
jinwoongBang <dlopo123@gmail.com>
2020-12-16 19:25:19 +0900

    완료/미완료 토글 함수에 대한 테스트 추가, 삭제 버튼 테스트 추가

--- src/test/TodoItem.test.js
+++ src/test/TodoItem.test.js
@@ -30,13 +30,28 @@ describe("<TodoItem />", () => {
     expect(button).toBeTruthy();
   });
 
+  // 완료된 Todo 항목에 대한 스타일 적용 테스트
   it("shows line-through on span when done is true", () => {
     const { span } = setup({ todo: { ...sampleTodo, done: true } });
     expect(span).toHaveStyle("text-decoration: line-through;");
   });
-
+  // 완료되지 않은 Todo 항목에 대한 스타일 적용 테스트
   it("does not show line-through on span when done is false", () => {
     const { span } = setup({ todo: { ...sampleTodo, done: false } });
     expect(span).not.toHaveStyle("text-decoration: line-through;");
+  });
+  // 완료/미완료 토글 테스트
+  it("calls onToggle", () => {
+    const onToggle = jest.fn();
+    const { span } = setup({ onToggle });
+    fireEvent.click(span);
+    expect(onToggle).toBeCalledWith(sampleTodo.id);
+  });
+  // Todo 항목 삭제 버튼 테스트
+  it("calls onRemove", () => {
+    const onRemove = jest.fn();
+    const { button } = setup({ onRemove });
+    fireEvent.click(button);
+    expect(onRemove).toBeCalledWith(sampleTodo.id);
   });
 });
