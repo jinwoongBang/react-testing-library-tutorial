f7894df24d815327d97b4f39fcc71e852b8b5ed6
jinwoongBang <dlopo123@gmail.com>
2020-12-22 22:10:29 +0900

    getByTestId() 함수를 사용하여 태그의 존재 유무를 확인하기 위한 data-testid 프로퍼티 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,10 @@ import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
-    <ul>
+    // querySelector 를 사용해서 찾을 수도있는데,
+    // 공식문서에서는 DOM API 로 선택하는 것 대신에 
+    // data-testid 를 사용 할 것을 권장
+    <ul data-testid="TodoList">
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
